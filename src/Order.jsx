bbf946a537170dc01e446f38294a6970fd9911a0
Casper Gamborg <Gamborgc@gmail.com>
2025-05-31 14:41:42 +0200

    Order component is done

---
+++ src/Order.jsx
@@ -0,0 +1,67 @@
+import Pizza from "./Pizza";
+
+export default function order() {
+  const pizzaType = "pepperoni";
+  const pizzaSize = "M";
+  return (
+    <div className="order">
+      <h2>Order page</h2>
+      <form action="">
+        <div>
+          <div>
+            <label htmlFor="pizza-type">Pizza type</label>
+            <select name="pizza-type" value={pizzaType}>
+              <option value="pepperione">The Pepperoini Pizza</option>
+              <option value="hawaiian">The Hawaiian Pizza</option>
+              <option value="big_meat">The Big Meat Pizza</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pizza-size">Pizza Size</label>
+            <div>
+              <span>
+                <input
+                  checked={pizzaSize === "S"}
+                  type="radio"
+                  name="pizza-size"
+                  value="s"
+                  id="pizza-s"
+                />
+                <label htmlFor="pizza-s">Small</label>
+              </span>
+              <span>
+                <input
+                  checked={pizzaSize === "M"}
+                  type="radio"
+                  name="pizza-size"
+                  value="m"
+                  id="pizza-m"
+                />
+                <label htmlFor="pizza-m">Medium</label>
+              </span>
+              <span>
+                <input
+                  checked={pizzaSize === "L"}
+                  type="radio"
+                  name="pizza-size"
+                  value="l"
+                  id="pizza-l"
+                />
+                <label htmlFor="pizza-l ">Large</label>
+              </span>
+            </div>
+          </div>
+          <button type="submit">Add to Cart</button>
+          <div className="order-pizza">
+            <Pizza
+              name="Pepperoni"
+              desription="Another pep pizza"
+              image="/public/pizzas/pepperoni.webp"
+            />
+            <p>$13.37</p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
