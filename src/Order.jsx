1a037d3695d5edd9a1de1c252075d30d4dec11af
Casper Gamborg <Gamborgc@gmail.com>
2025-05-31 19:16:02 +0200

    Computer change

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
 import Pizza from "./Pizza";
 
-export default function order() {
-  const pizzaType = "pepperoni";
-  const pizzaSize = "M";
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export default function Order() {
+  //   const pizzaType = "pepperoni";
+  //   const pizzaSize = "M";
+  const [pizzaTypes, setPizzaTypes] = useState([]);
+  const [pizzaType, setPizzaType] = useState("pepperoni");
+  const [pizzaSize, setPizzaSize] = useState("M");
+  const [loading, setLoading] = useState(true);
+
+  let price, selectedPizza;
+
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+    price = intl.format(selectedPizza.sizes[pizzaSize]);
+  }
+
+  async function fetchPizzaTypes() {
+    const pizzaRes = await fetch("/api/pizzas");
+    const pizzaJson = await pizzaRes.json();
+    setPizzaTypes(pizzaJson);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []);
+
   return (
     <div className="order">
       <h2>Order page</h2>
@@ -10,10 +39,16 @@ export default function order() {
         <div>
           <div>
             <label htmlFor="pizza-type">Pizza type</label>
-            <select name="pizza-type" value={pizzaType}>
-              <option value="pepperione">The Pepperoini Pizza</option>
-              <option value="hawaiian">The Hawaiian Pizza</option>
-              <option value="big_meat">The Big Meat Pizza</option>
+            <select
+              onChange={(e) => setPizzaType(e.target.value)}
+              name="pizza-type"
+              value={pizzaType}
+            >
+              {pizzaTypes.map((pizza) => (
+                <option key={pizza.id} value={pizza.id}>
+                  {pizza.name}
+                </option>
+              ))}
             </select>
           </div>
           <div>
@@ -24,8 +59,9 @@ export default function order() {
                   checked={pizzaSize === "S"}
                   type="radio"
                   name="pizza-size"
-                  value="s"
+                  value="S"
                   id="pizza-s"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-s">Small</label>
               </span>
@@ -34,8 +70,9 @@ export default function order() {
                   checked={pizzaSize === "M"}
                   type="radio"
                   name="pizza-size"
-                  value="m"
+                  value="M"
                   id="pizza-m"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-m">Medium</label>
               </span>
@@ -44,21 +81,27 @@ export default function order() {
                   checked={pizzaSize === "L"}
                   type="radio"
                   name="pizza-size"
-                  value="l"
+                  value="L"
                   id="pizza-l"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
-                <label htmlFor="pizza-l ">Large</label>
+                <label htmlFor="pizza-l">Large</label>
               </span>
             </div>
           </div>
           <button type="submit">Add to Cart</button>
           <div className="order-pizza">
-            <Pizza
-              name="Pepperoni"
-              desription="Another pep pizza"
-              image="/public/pizzas/pepperoni.webp"
-            />
-            <p>$13.37</p>
+            {loading ? (
+              <h1>Loading pizza lol</h1>
+            ) : (
+              <Pizza
+                name={selectedPizza.name}
+                desription={selectedPizza.desription}
+                image={selectedPizza.image}
+              />
+            )}
+
+            <p>{price}</p>
           </div>
         </div>
       </form>
